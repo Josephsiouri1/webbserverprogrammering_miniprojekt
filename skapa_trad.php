21fb8b9ea309a34f86a87baf87d6291069984b7d
Joseph Siouri <joseph.siouri@skola.taby.se>
2022-10-07 20:51:46 +0200

    changes

--- skapa_trad.php
+++ skapa_trad.php
@@ -20,6 +20,8 @@ $result = $conn->query($sql_tradar);
 if ($rubrik && $inlagg) {
     $sql = "INSERT INTO tradar (id, rubrik, skapad_av, senaste_inlagg) VALUES ($result->num_rows, '$rubrik', '$skapad_av', NOW())";
     $result = $conn->query($sql);
+    $sql = "INSERT INTO inlagg (id, skriven_av, kommentar, datum) VALUES ($result->num_rows+1, '$skapad_av', '$inlagg', NOW())";
+    $result = $conn->query($sql);
     header('Location: ' . './bekraftat_inlagg.php');
 } else {
     header('Location: ' . './fel_med_inlagg.php');
